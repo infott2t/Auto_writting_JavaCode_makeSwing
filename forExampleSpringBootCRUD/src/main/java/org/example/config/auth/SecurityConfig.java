63b58133ea528f89efbe7f43fa7a07e81a94beb7
HYEONIL CHOI <tt2t2am1118@naver.com>
2021-08-29 20:32:26 +0900

    2021-08-29 일요일 20:29. Create file.
    SecurityConfig.java file.
    and, this time hasRole USER, /api/v1/**.
    -- 지금 USER의 경우에는 api/v1/**를 다 들어갈 수 있는데요. ADMIN역할을 만들어서, 데이터베이스 접근을 한정시키게 바꾸어야하겠죠. 서비스제공자의 입장에서, 쇼핑몰 제품 가격이라든지... 편집할 때라든지요. 공부해보세요. http://spring.io
    스프링부트 웹프로그래밍이군요. 
    
    Let's study. https://spring.io About Spring Boot, Web Programming.
    
    Thank you.

---
+++ forExampleSpringBootCRUD/src/main/java/org/example/config/auth/SecurityConfig.java
@@ -0,0 +1,37 @@
+package org.example.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and()
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+
+    }
+
+
+
+
+}
