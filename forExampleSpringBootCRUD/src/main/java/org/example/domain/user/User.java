a34d9062b253855dedda48bc44827d8ba8248b77
HYEONIL CHOI <tt2t2am1118@naver.com>
2021-08-29 20:47:36 +0900

    2021-08-29 일요일 20:47 File create.
    org.example.domain.user.User.java file.

---
+++ forExampleSpringBootCRUD/src/main/java/org/example/domain/user/User.java
@@ -0,0 +1,57 @@
+package org.example.domain.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.domain.BaseTimeEntity;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role){
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User(String name, String email, String picture){
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+
+    public User update(String name, String picture){
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+}
